c56626d44b2157d3dd94b6099cf7a6482122fd85
Thibaut Baguette <thibaut.baguette@mail.mcgill.ca>
2023-10-02 18:14:50 -0400

    Update CV

--- js/global.js
+++ js/global.js
@@ -1,17 +1,14 @@
-const email = 'thibaut.baguette@mail.mcgill.ca';
+const email = "thibaut.baguette@mail.mcgill.ca";
 
-var currentLang = $('html').attr('lang');
-var switchToLang = 'NA';
-if (currentLang === 'fr') {
-  switchToLang = 'en';
-} else if (currentLang === 'en') {
-  switchToLang = 'fr';
+var currentLang = $("html").attr("lang");
+var switchToLang = "NA";
+if (currentLang === "fr") {
+  switchToLang = "en";
+} else if (currentLang === "en") {
+  switchToLang = "fr";
 }
 
-var cvPath = `/cv/Thibaut Baguette's resume.pdf`;
-if (currentLang === 'fr') {
-  cvPath = `/cv/CV de Thibaut Baguette.pdf`;
-}
+var cvPath = `/cv/ThibautBaguette_CV.pdf`;
 
 function switchLang() {
   var url = window.location.href;
@@ -20,21 +17,21 @@ function switchLang() {
 }
 
 $(function () {
-  $('.cv-image').attr('title', 'Click to Enlarge Image!');
+  $(".cv-image").attr("title", "Click to Enlarge Image!");
 
-  $('.cv-image').click(function () {
-    if ($(this).hasClass('focus')) {
-      $(this).removeClass('focus');
-      $('#background-blur').remove();
+  $(".cv-image").click(function () {
+    if ($(this).hasClass("focus")) {
+      $(this).removeClass("focus");
+      $("#background-blur").remove();
     } else {
-      $('.cv-image').removeClass('top');
-      $(this).addClass('top');
-      $(this).addClass('focus');
-      var el = document.createElement('div');
-      el.id = 'background-blur';
-      $('body').prepend(el);
+      $(".cv-image").removeClass("top");
+      $(this).addClass("top");
+      $(this).addClass("focus");
+      var el = document.createElement("div");
+      el.id = "background-blur";
+      $("body").prepend(el);
     }
-  })
-})
+  });
+});
 
 export { email, cvPath, currentLang, switchLang };
